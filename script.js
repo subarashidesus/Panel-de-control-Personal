4ac1535319ae430f3ed76869cc60d9295f7e204c
GusGus <134746058+subarashidesus@users.noreply.github.com>
2024-02-26 11:32:15 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,56 @@
-const menuOpen = document.getElementById('menu-open');
-const menuClose = document.getElementById('menu-close');
-const sideBar = document.querySelector('.container .left-section');
-const sidebarItems = document.querySelectorAll('.container .left-section .sidebar .item');
+const sideLinks = document.querySelectorAll('.sidebar .side-menu li a:not(.logout)');
 
-menuOpen.addEventListener('click', () => {
-    sideBar.style.top = '0';
+sideLinks.forEach(item => {
+    const li = item.parentElement;
+    item.addEventListener('click', () => {
+        sideLinks.forEach(i => {
+            i.parentElement.classList.remove('active');
+        })
+        li.classList.add('active');
+    })
 });
 
-menuClose.addEventListener('click', () => {
-    sideBar.style.top = '-60vh';
+const menuBar = document.querySelector('.content nav .bx.bx-menu');
+const sideBar = document.querySelector('.sidebar');
+
+menuBar.addEventListener('click', () => {
+    sideBar.classList.toggle('close');
 });
 
-let activeItem = sidebarItems[0];
+const searchBtn = document.querySelector('.content nav form .form-input button');
+const searchBtnIcon = document.querySelector('.content nav form .form-input button .bx');
+const searchForm = document.querySelector('.content nav form');
 
-sidebarItems.forEach(element => {
-    element.addEventListener('click', () => {
-        if(activeItem){
-            activeItem.removeAttribute('id');
+searchBtn.addEventListener('click', function (e) {
+    if (window.innerWidth < 576) {
+        e.preventDefault;
+        searchForm.classList.toggle('show');
+        if (searchForm.classList.contains('show')) {
+            searchBtnIcon.classList.replace('bx-search', 'bx-x');
+        } else {
+            searchBtnIcon.classList.replace('bx-x', 'bx-search');
         }
+    }
+});
 
-        element.setAttribute('id', 'active');
-        activeItem = element;
-    });
+window.addEventListener('resize', () => {
+    if (window.innerWidth < 768) {
+        sideBar.classList.add('close');
+    } else {
+        sideBar.classList.remove('close');
+    }
+    if (window.innerWidth > 576) {
+        searchBtnIcon.classList.replace('bx-x', 'bx-search');
+        searchForm.classList.remove('show');
+    }
+});
+
+const toggler = document.getElementById('theme-toggle');
+
+toggler.addEventListener('change', function () {
+    if (this.checked) {
+        document.body.classList.add('dark');
+    } else {
+        document.body.classList.remove('dark');
+    }
 });
